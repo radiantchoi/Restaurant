d17781e0d9e55afbb797ad2f63b8c1d1670802b1
Gordon Choi <thefirate@gmail.com>
2021-06-15 01:52:11 +0900

    Add codes to parse the responses

--- Restaurant/ViewControllers/MenuController.swift
+++ Restaurant/ViewControllers/MenuController.swift
@@ -15,7 +15,13 @@ class MenuController {
         let categoryURL = baseURL.appendingPathComponent("categories")
         
         let task = URLSession.shared.dataTask(with: categoryURL) { (data, response, error) in
-            
+            if let data = data,
+               let jsonDictionary = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+               let categories = jsonDictionary["categories"] as? [String] {
+                completion(categories)
+            } else {
+                completion(nil)
+            }
         }
         
         task.resume()
@@ -28,7 +34,13 @@ class MenuController {
         let menuURL = components.url!
         
         let task = URLSession.shared.dataTask(with: menuURL) { (data, response, error) in
-            
+            let jsonDecoder = JSONDecoder()
+            if let data = data,
+               let menuItems = try? jsonDecoder.decode(MenuItems.self, from: data) {
+                completion(menuItems.items)
+            } else {
+                completion(nil)
+            }
         }
         
         task.resume()
@@ -45,8 +57,14 @@ class MenuController {
         let jsonData = try? jsonEncoder.encode(data)
         request.httpBody = jsonData
         
-        let task = URLSession.shared.dataTask(with: request) { (data, request, error) in
-            
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            if let data = data,
+               let preparationTime = try? jsonDecoder.decode(PreparationTime.self, from: data) {
+                completion(preparationTime.prepTime)
+            } else {
+                completion(nil)
+            }
         }
         task.resume()
     }
