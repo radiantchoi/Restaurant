d102baf00f4fdb3d94becd6bdb059eecf3b84aea
Gordon Choi <thefirate@gmail.com>
2021-06-15 03:09:52 +0900

    Display menus on individual menu screen

--- Restaurant/ViewControllers/MenuTableViewController.swift
+++ Restaurant/ViewControllers/MenuTableViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class MenuTableViewController: UITableViewController {
     
+    private let menuController = MenuController()
+    private var menuItems = [MenuItem]()
+    
     var category: String!
     
 }
@@ -17,7 +20,30 @@ extension MenuTableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = category.capitalized
+        menuController.fetchMenuItems(forCategory: category) { (menuItems) in
+            if let menuItems = menuItems {
+                self.updateUI(with: menuItems)
+            }
+        }
+    }
+    
+}
 
+extension MenuTableViewController {
+    
+    private func updateUI(with menuItems: [MenuItem]) {
+        DispatchQueue.main.async {
+            self.menuItems = menuItems
+            self.tableView.reloadData()
+        }
+    }
+    
+    private func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let menuItem = menuItems[indexPath.row]
+        cell.textLabel?.text = menuItem.name
+        cell.detailTextLabel?.text = "$\(menuItem.price)"
     }
     
 }
@@ -25,24 +51,19 @@ extension MenuTableViewController {
 extension MenuTableViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return menuItems.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCellIdentifier", for: indexPath)
+        configure(cell, forItemAt: indexPath)
         return cell
     }
-     */
+
 
     /*
 
