f4075b1f714a1cd8e0e2b6b961689a94a1ba3036
Gordon Choi <thefirate@gmail.com>
2021-06-15 01:41:41 +0900

    Build data flow

--- Restaurant/ViewControllers/MenuController.swift
+++ Restaurant/ViewControllers/MenuController.swift
@@ -9,17 +9,45 @@ import Foundation
 
 class MenuController {
     
-    let baseURL = URL(string: "http://localhost:8090/")
+    let baseURL = URL(string: "http://localhost:8090/")!
     
     func fetchCategories(completion: @escaping ([String]?) -> Void) {
-        let categoryURL = baseURL?.appendingPathComponent("categories")
+        let categoryURL = baseURL.appendingPathComponent("categories")
+        
+        let task = URLSession.shared.dataTask(with: categoryURL) { (data, response, error) in
+            
+        }
+        
+        task.resume()
     }
     
     func fetchMenuItems(forCategory categoryName: String, completion: @escaping ([MenuItem]?) -> Void) {
+        let initialMenuURL = baseURL.appendingPathComponent("menu")
+        var components = URLComponents(url: initialMenuURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
+        let menuURL = components.url!
         
+        let task = URLSession.shared.dataTask(with: menuURL) { (data, response, error) in
+            
+        }
+        
+        task.resume()
     }
     
     func submitOrder(forMenuIDs menuIds: [Int], completion: @escaping (Int?) -> Void) {
-        let orderURL = baseURL?.appendingPathComponent("order")
+        let orderURL = baseURL.appendingPathComponent("order")
+        var request = URLRequest(url: orderURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let data: [String: [Int]] = ["menuIds": menuIds]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(data)
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, request, error) in
+            
+        }
+        task.resume()
     }
 }
