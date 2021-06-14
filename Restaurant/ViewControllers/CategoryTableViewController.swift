f10e3bf79db091f7da4de4960e7d7c70bb0ae30a
Gordon Choi <thefirate@gmail.com>
2021-06-15 02:51:04 +0900

    Downcast category data to menu view controller

--- Restaurant/ViewControllers/CategoryTableViewController.swift
+++ Restaurant/ViewControllers/CategoryTableViewController.swift
@@ -59,14 +59,13 @@ extension CategoryTableViewController {
         return cell
     }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "MenuSegue" {
+            let menuTableViewController = segue.destination as! MenuTableViewController
+            let index = tableView.indexPathForSelectedRow!.row
+            menuTableViewController.category = categories[index]
+        }
     }
-    */
 
 }
