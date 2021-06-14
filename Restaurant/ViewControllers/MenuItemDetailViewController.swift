6450ddbe662051457b78abd0a0ec0645daed83d5
Gordon Choi <thefirate@gmail.com>
2021-06-15 04:14:07 +0900

    Display menu details (not yet image)

--- Restaurant/ViewControllers/MenuItemDetailViewController.swift
+++ Restaurant/ViewControllers/MenuItemDetailViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class MenuItemDetailViewController: UIViewController {
     
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var priceLabel: UILabel!
+    @IBOutlet private var detailTextLabel: UILabel!
+    @IBOutlet private var addToOrderButton: UIButton!
+    
     var menuItem: MenuItem!
     
 }
@@ -17,8 +23,18 @@ extension MenuItemDetailViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
+    }
+    
+}
 
-
+extension MenuItemDetailViewController {
+    
+    private func updateUI() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        detailTextLabel.text = menuItem.detailText
     }
     
 }
