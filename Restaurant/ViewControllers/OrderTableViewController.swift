984b9159a0cf1eb3883dd764103e05d610fac69c
Gordon Choi <thefirate@gmail.com>
2021-06-16 03:07:36 +0900

    Now swipe to delete order

--- Restaurant/ViewControllers/OrderTableViewController.swift
+++ Restaurant/ViewControllers/OrderTableViewController.swift
@@ -15,6 +15,7 @@ extension OrderTableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
         
         NotificationCenter.default.addObserver(tableView, selector: #selector(UITableView.reloadData), name: MenuController.orderUpdatedNotification, object: nil)
     }
@@ -48,26 +49,15 @@ extension OrderTableViewController {
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            MenuController.shared.order.menuItems.remove(at: indexPath.row)
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
