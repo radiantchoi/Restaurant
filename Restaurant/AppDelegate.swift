83399c742aa10c67b1a4af81c3b485c9be56af96
Gordon Choi <thefirate@gmail.com>
2021-06-16 23:36:39 +0900

    Build to save and load the order

--- Restaurant/AppDelegate.swift
+++ Restaurant/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 extension AppDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        MenuController.shared.loadOrder()
         return true
     }
 
@@ -33,7 +33,10 @@ extension AppDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        MenuController.shared.saveOrder()
+    }
+    
 }
 
