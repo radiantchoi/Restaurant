ab1c237deef507d3a505cdd81b9c0be7d35f0ebe
Gordon Choi <thefirate@gmail.com>
2021-06-16 23:40:02 +0900

    Move save order code to right position

--- Restaurant/AppDelegate.swift
+++ Restaurant/AppDelegate.swift
@@ -15,11 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension AppDelegate {
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        MenuController.shared.loadOrder()
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        MenuController.shared.saveOrder()
         return true
     }
-
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        MenuController.shared.saveOrder()
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -33,10 +41,6 @@ extension AppDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        MenuController.shared.saveOrder()
-    }
-    
+
 }
 
