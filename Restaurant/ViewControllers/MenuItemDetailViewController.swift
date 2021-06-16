9934dd7958d61fc200963a940bd7f4a231e5ace9
Gordon Choi <thefirate@gmail.com>
2021-06-16 19:54:02 +0900

    Carry image for menu detail view

--- Restaurant/ViewControllers/MenuItemDetailViewController.swift
+++ Restaurant/ViewControllers/MenuItemDetailViewController.swift
@@ -36,6 +36,12 @@ extension MenuItemDetailViewController {
         titleLabel.text = menuItem.name
         priceLabel.text = String(format: "$%.2f", menuItem.price)
         detailTextLabel.text = menuItem.detailText
+        MenuController.shared.fetchImage(url: menuItem.imageURL) { (image) in
+            guard let image = image else { return }
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
     }
     
 }
