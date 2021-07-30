842ff274174d3ba69a9d2a350c1b3b9cf858dd1f
Gordon Choi <thefirate@gmail.com>
2021-07-31 00:16:03 +0900

    Clean up commented codes

--- Restaurant/Application/Networking/NetworkManager.swift
+++ Restaurant/Application/Networking/NetworkManager.swift
@@ -15,72 +15,6 @@ class NetworkManager {
     
 }
 
-//extension NetworkManager {
-//    @discardableResult
-//    func post<T: Codable>(NetworkRequestData: NetworkRequestData, for model: T.Type, completion: @escaping (Result<T, Error>) -> Void) -> URLSessionDataTask {
-//
-//        // url의 설정 - 미리 설정해둔 baseURL에 붙인다
-//        let URL = NetworkRequestData.baseURL.appendingPathComponent(NetworkRequestData.urlPath)
-//        var request = URLRequest(url: URL)
-//
-//        // 일단 HTTPMethod 자리를 내 준다.
-//        request.httpMethod = NetworkRequestData.httpMethod.rawValue
-//
-//        // 그리고 그 내 준 자리를 채워 준다 - header
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//
-//        let jsonData = try? JSONSerialization.data(withJSONObject: NetworkRequestData.data!)
-//
-//        // 이게 post인 경우는 httpBody에 관련된 정보가 들어가지만, get일 경우는 query에 정보가 들어가겠지? switch를 통해 나중에 함수를 하나로 통합할 수 있다.
-//        request.httpBody = jsonData
-//
-//        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-//            guard let data = data,
-//                  let model = try? JSONDecoder().decode(model, from: data) else {
-//                // 이 밑은 데이터를 가져오는 데 실패했을 경우
-//                if let error = error {
-//                    completion(.failure(error))
-//                } else {
-//                    completion(.failure(UnknownNetworkError()))
-//                }
-//                return
-//            }
-//
-//            // 데이터를 가져오는 데 성공했을 경우
-//            completion(.success(model))
-//        }
-//
-//        task.resume()
-//        return task
-//    }
-//
-//    func get<T: Codable>(NetworkRequestData: NetworkRequestData, for model: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
-//
-//        let URL = NetworkRequestData.baseURL.appendingPathComponent(NetworkRequestData.urlPath)
-//        var request = URLRequest(url: URL)
-//
-//        request.httpMethod = NetworkRequestData.httpMethod.rawValue
-//
-//        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-//            guard let data = data,
-//                  let model = try? JSONDecoder().decode(model, from: data) else {
-//                if let error = error {
-//                    completion(.failure(error))
-//                } else {
-//                    completion(.failure(UnknownNetworkError()))
-//                }
-//                return
-//            }
-//
-//            completion(.success(model))
-//        }
-//
-//        task.resume()
-//
-//    }
-//
-//}
-
 extension NetworkManager {
     @discardableResult
     func request<T: Codable>(networkRequestData: NetworkRequestData, for model: T.Type, completion: @escaping (Result<T, Error>) -> Void) -> URLSessionDataTask {
@@ -94,10 +28,6 @@ extension NetworkManager {
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             let jsonData = try? JSONSerialization.data(withJSONObject: networkRequestData.data!)
             request.httpBody = jsonData
-//            let data = networkRequestData.data
-//            let jsonEncoder = JSONEncoder()
-//            let jsonData = try? jsonEncoder.encode(data)
-//            request.httpBody = jsonData
             
         }
         
@@ -108,7 +38,6 @@ extension NetworkManager {
                     completion(.failure(error))
                 } else {
                     completion(.failure(UnknownNetworkError()))
-                    // Error while submitting order
                 }
                 return
             }
--- Restaurant/Application/ViewControllers/MenuController.swift
+++ Restaurant/Application/ViewControllers/MenuController.swift
@@ -39,27 +39,6 @@ extension MenuController {
     
     func submitOrder(forMenuIDs menuIds: [Int], completion: @escaping (Int?) -> Void) {
         
-//        let orderURL = baseURL.appendingPathComponent("order")
-//        var request = URLRequest(url: orderURL)
-//        request.httpMethod = "POST"
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//        
-//        let data: [String: [Int]] = ["menuIds": menuIds]
-//        let jsonEncoder = JSONEncoder()
-//        let jsonData = try? jsonEncoder.encode(data)
-//        request.httpBody = jsonData
-//        
-//        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-//            let jsonDecoder = JSONDecoder()
-//            if let data = data,
-//               let preparationTime = try? jsonDecoder.decode(PreparationTime.self, from: data) {
-//                completion(preparationTime.prepTime)
-//            } else {
-//                completion(nil)
-//            }
-//        }
-//        task.resume()
-        
         NetworkManager.shared.request(networkRequestData: NetworkRequestData(
                                     urlPath: "order",
                                     httpMethod: .post,
@@ -71,24 +50,9 @@ extension MenuController {
                 completion(preparationTime.prepTime)
             case .failure(_):
                 completion(nil)
-                // Error
             }
         }
     }
-    
-//    func fetchImage(url: URL, completion: @escaping (UIImage?) -> Void) {
-//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-//            if let data = data,
-//                let image = UIImage(data: data) {
-//                completion(image)
-//            } else {
-//                completion(nil)
-//            }
-//        }
-//        
-//        task.resume()
-//        
-//    }
     
     private func process(_ items: [MenuItem]) {
         itemsByID.removeAll()
@@ -105,18 +69,6 @@ extension MenuController {
     }
     
     func loadRemoteData() {
-//        let initialMenuURL = baseURL.appendingPathComponent("menu")
-//        let components = URLComponents(url: initialMenuURL, resolvingAgainstBaseURL: true)!
-//        let menuURL = components.url!
-//
-//        let task = URLSession.shared.dataTask(with: menuURL) { (data, _, _) in
-//            let jsonDecoder = JSONDecoder()
-//            if let data = data, let menuItems = try? jsonDecoder.decode(MenuItems.self, from: data) {
-//                self.process(menuItems.items)
-//            }
-//        }
-//
-//        task.resume()
         
         NetworkManager.shared.request(networkRequestData: NetworkRequestData(
                                     urlPath: "menu",
@@ -147,48 +99,3 @@ extension MenuController {
     }
     
 }
-
-
-extension MenuController {
-    
-//    func loadOrder() {
-//        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-//        let orderFileURL = documentsDirectoryURL.appendingPathComponent("order").appendingPathExtension("json")
-//        
-//        guard let data = try? Data(contentsOf: orderFileURL) else { return }
-//        order = (try? JSONDecoder().decode(Order.self, from: data)) ?? Order(menuItems: [])
-//    }
-//    
-//    func saveOrder() {
-//        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-//        let orderFileURL = documentsDirectoryURL.appendingPathComponent("order").appendingPathExtension("json")
-//        
-//        if let data = try? JSONEncoder().encode(order) {
-//            try? data.write(to: orderFileURL)
-//        }
-//    }
-//    
-//    func loadItems() {
-//        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-//        let menuItemsFileURL = documentsDirectoryURL.appendingPathComponent("menuItems").appendingPathExtension("json")
-//        
-//        guard let data = try? Data(contentsOf: menuItemsFileURL) else { return }
-//        let items = (try? JSONDecoder().decode([MenuItem].self, from: data)) ?? []
-//        
-//        process(items)
-//        
-//    }
-//    
-//    func saveItems() {
-//        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-//        let menuItemsFileURL = documentsDirectoryURL.appendingPathComponent("menuItems").appendingPathExtension("json")
-//        
-//        let items = Array(itemsByID.values)
-//        if let data = try? JSONEncoder().encode(items) {
-//            try? data.write(to: menuItemsFileURL)
-//        }
-//    }
-    
-}
-
-
